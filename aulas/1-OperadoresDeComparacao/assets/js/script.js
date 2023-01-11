d854d53f65785df750864cebde74e645a0229469
ICEBERG776 <henriqueau111@gmail.com>
2023-01-10 22:28:47 -0300

    first commit

---
+++ aulas/1-OperadoresDeComparacao/assets/js/script.js
@@ -0,0 +1,19 @@
+/*
+Operadores de comparação
+
+> maior que
+>= maior que ou igual a 
+< menor que
+<= menor que ou igual a
+== igualdade (valor) *************
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) *************
+!== diferente estrito (valor e tipo)
+*/
+
+const num1 = 10; // number
+const num2 = "10"; // string
+
+
+const comp = num1 !== num2;
+console.log(comp); // Vai retornar um boolean
---
+++ aulas/2-OperadoresLogicos/assets/js/script.js
@@ -0,0 +1,17 @@
+/*
+Operadores Lógicos
+&& -> AND -> E // Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU 
+! -> NOT -> NÃO
+*/
+
+// const expressaoAnd = true && true && true && true && true;
+// const expressaoOr = false || false || true || false;
+// console.log(expressaoOr) 
+
+// const usuario = "Luiz"; // Form usuário digitou
+// const senha = "123456"; // Form usuário digitou
+//                            true                false
+// const vaiLogar = usuario === "Luiz" && senha === "12345";
+
+console.log(!!true)
