fdca42807ea2eb892c45047e028eb1a8c6e100a0
ICEBERG776 <henriqueau111@gmail.com>
2023-01-12 22:42:26 -0300

    new file:   aulas/4-IfElse/assets/js/script.js 	new file:   aulas/4-IfElse/assets/readme 	new file:   aulas/4-IfElse/index.html

---
+++ aulas/4-IfElse/assets/js/script.js
@@ -0,0 +1,33 @@
+/*
+Entre 0 - 11 - Bom dia
+Entre 12 - 17 - Boa tarde
+Entre 18 - 23 - Boa noite
+*/
+
+// If pode ser usado sozinho
+// Sempre que utilizo a palavra else, preciso de um if antes
+// Eu posso ter vários else ifs na chegagem
+// Só posso ter um else checagem na condição
+
+// const hora = 50;
+
+// if (hora >= 7 && hora <= 11 ){
+//     console.log("Bom dia");
+
+// }else if (hora >= 12 && hora <= 17){
+//     console.log("Boa tarde");
+// }else if (hora >= 18 && hora <= 23){
+//     console.log("Boa noite");
+// }else if (hora >= 0 && hora <= 6) {
+//     console.log("Vai dormir");
+// } else {
+//     console.log("Tenha uma boa semana!")
+// }
+
+const tenhoGrana = NaN;
+
+if (tenhoGrana) {
+    console.log("Vou sair de casa");
+} else {
+    console.log("Não vou sair de casa")
+}
